c77c9f9348236385e802bbc3413473b75713bf01
Arm1K24 <61420412+Arm1K24@users.noreply.github.com>
2025-03-23 05:08:10 +0300

    Update CachePlugin.java

--- android/src/main/java/com/example/cache/CachePlugin.java
+++ android/src/main/java/com/example/cache/CachePlugin.java
@@ -1,16 +1,11 @@
 package com.example.cache;
 
-import android.Manifest;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
-
-import com.tbruyelle.rxpermissions.RxPermissions;
-
-import java.util.HashMap;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -19,41 +14,21 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/**
- * CachePlugin
- */
+import com.tbruyelle.rxpermissions3.RxPermissions;
+
+import java.util.HashMap;
+
 public class CachePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
     private Activity activity;
     private RxPermissions rxPermissions;
 
-
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-
-        channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "cache");
-
+        // Инициализация MethodChannel с использованием BinaryMessenger
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "cache");
         channel.setMethodCallHandler(this);
-    }
-
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "cache");
-        channel.setMethodCallHandler(new CachePlugin());
     }
 
     @Override
@@ -74,7 +49,7 @@ public class CachePlugin implements FlutterPlugin, MethodCallHandler, ActivityAw
             case "availableSpace":
                 rxPermissions = new RxPermissions(activity);
                 rxPermissions
-                        .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .request(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         .subscribe(granted -> {
                             if (granted) {
                                 result.success(PhoneUtil.getInstance().getSDFreeSize() + "MB");
@@ -86,7 +61,7 @@ public class CachePlugin implements FlutterPlugin, MethodCallHandler, ActivityAw
             case "deviceCacheSpace":
                 rxPermissions = new RxPermissions(activity);
                 rxPermissions
-                        .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .request(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         .subscribe(granted -> {
                             if (granted) {
                                 result.success(PhoneUtil.getInstance().getSDAllSize() + "MB");
@@ -101,10 +76,11 @@ public class CachePlugin implements FlutterPlugin, MethodCallHandler, ActivityAw
                 byte[] data = (byte[]) hashMap.get("data");
                 rxPermissions = new RxPermissions(activity);
                 rxPermissions
-                        .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .request(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         .subscribe(granted -> {
                             if (granted) {
                                 FileUtil.saveBitmap(path, Bytes2Bimap(data));
+                                result.success("Image saved");
                             } else {
                                 result.success("无访问权限");
                             }
@@ -113,11 +89,10 @@ public class CachePlugin implements FlutterPlugin, MethodCallHandler, ActivityAw
             default:
                 result.notImplemented();
                 break;
-
         }
     }
 
-    public Bitmap Bytes2Bimap(byte[] b) {
+    private Bitmap Bytes2Bimap(byte[] b) {
         if (b.length != 0) {
             return BitmapFactory.decodeByteArray(b, 0, b.length);
         } else {
@@ -127,28 +102,30 @@ public class CachePlugin implements FlutterPlugin, MethodCallHandler, ActivityAw
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-        channel.setMethodCallHandler(new CachePlugin());
-        Log.d("onDetachedFromEngine", "onDetachedFromEngine");
+        channel.setMethodCallHandler(null); // Очистка обработчика при отключении
+        Log.d("CachePlugin", "onDetachedFromEngine");
     }
 
     @Override
-    public void onAttachedToActivity(ActivityPluginBinding binding) {
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         activity = binding.getActivity();
-        Log.d("onAttachedToActivity", "onAttachedToActivity" + (activity != null));
+        Log.d("CachePlugin", "onAttachedToActivity: " + (activity != null));
     }
 
     @Override
     public void onDetachedFromActivityForConfigChanges() {
-        Log.d("onDetachedFromEngine", "onDetachedFromEngine");
+        Log.d("CachePlugin", "onDetachedFromActivityForConfigChanges");
     }
 
     @Override
-    public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activity = binding.getActivity();
+        Log.d("CachePlugin", "onReattachedToActivityForConfigChanges");
     }
 
     @Override
     public void onDetachedFromActivity() {
-        Log.d("onDetachedFromActivity", "onDetachedFromActivity");
+        activity = null;
+        Log.d("CachePlugin", "onDetachedFromActivity");
     }
-
 }
